d16c2ceebb009189a08b1c8e6db190169653e8be
sabbir ahmmed <ahsabbir103@gmail.com>
2024-06-21 11:59:38 +0600

    rendering issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import LandingPage from './components/LandingPage'
 import Navbar from './components/Navbar'
@@ -8,8 +9,17 @@ import Features from './components/Features'
 import { Card } from './components/Cards'
 import { Ready } from './components/Ready'
 import { Footer } from './components/Footer'
+import LocomotiveScroll from 'locomotive-scroll';
+
+//import dynamic from 'next/dynamic'
+ 
+//const DynamicHeader = dynamic(() => import('../components/header'), {
+//  ssr: false,
+//})
+
 
 function Home() {
+const locomotiveScroll = new LocomotiveScroll()
   return (
     <div className='w-full min-h-screen text-white bg-zinc-900'>
       <Navbar/>
